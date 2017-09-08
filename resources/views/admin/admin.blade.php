ebaa676a7391a328b5ca56073b47c2b15954fc70
anclaan <anclaan@gmail.com>
2017-09-08 14:54:07 +0200

    zmiana druga

--- resources/views/admin/admin.blade.php
+++ resources/views/admin/admin.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>Plastico Admin</title>
+    <title>Plastico Admin!</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Bootstrap -->
     <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
